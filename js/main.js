4979f02fbbda1acd2c119926d8247869c8cacfc2
pervakam <52745442+pervakam@users.noreply.github.com>
2021-01-27 08:47:12 +0300

    добавляет ссылки и слайдер-карусель

--- js/main.js
+++ js/main.js
@@ -14,10 +14,12 @@ const pageHeader = document.querySelector('.page-header');
 const mainNavHelpItemIcons = document.querySelectorAll('.main-nav__help-item-icon--menu');
 const mainNavHelpItemDescriptions = document.querySelectorAll('.main-nav__help-item-description');
 
+///// массивы для ie /////
 const headerMainMenuItemsArray = Array.prototype.slice.call(headerMainMenuItems);
 const mainNavHelpItemIconsArray = Array.prototype.slice.call(mainNavHelpItemIcons);
 const mainNavHelpItemDescriptionsArray = Array.prototype.slice.call(mainNavHelpItemDescriptions);
 
+///// открывает/закрывает меню в шапке /////
 const closeHeaderMenu = function () {
   headerMenu.classList.add('page-header__nav-hide');
   headerMenuButton.classList.remove('main-nav__toggle--close');
@@ -57,11 +59,11 @@ const closeHeaderMenuByClick = function () {
 
 const openHeaderMenu = function () {
   headerMenu.classList.remove('page-header__nav-hide');
-  headerMenuButton.classList.add('main-nav__toggle--close');
-  headerTopSection.classList.add('page-header__nav-hide');
-  headerCitySection.classList.add('page-header__nav-hide');
   mainNavButton.classList.remove('page-header__nav-hide');
   mainNavFirstItem.classList.remove('page-header__nav-hide');
+  headerTopSection.classList.add('page-header__nav-hide');
+  headerCitySection.classList.add('page-header__nav-hide');
+  headerMenuButton.classList.add('main-nav__toggle--close');
   mainNav.classList.add('main-nav-menu');
   mainNavHelpList.classList.add('main-nav__help-list--menu');
   headerMainMenuItemsArray.forEach(function (it) {
@@ -73,9 +75,7 @@ const openHeaderMenu = function () {
   mainNavHelpItemDescriptionsArray.forEach(function (it) {
     it.classList.add('main-nav__help-item-description--menu');
   });
-
   closeHeaderMenuByClick();
-
   window.removeEventListener('resize', changeTabletMenu);
 };
 
@@ -84,6 +84,7 @@ headerMenuButton.addEventListener('click', function (evt) {
   openHeaderMenu();
 });
 
+///// прокрутка наверх /////
 window.addEventListener('scroll', function () {
   const scrolled = window.pageYOffset || document.documentElement.scrollTop;
   if (scrolled > 100) {
@@ -101,6 +102,7 @@ upButton.addEventListener('click', function (evt) {
   });
 });
 
+///// меню в шапке меняет для модалки /////
 const changeTabletMenu = function () {
   if (window.innerWidth <= 1279) {
     mainNavFirstItem.classList.add('page-header__nav-hide');
@@ -113,12 +115,41 @@ changeTabletMenu();
 
 window.addEventListener('resize', changeTabletMenu);
 
+///// слайдер в ОМС /////
+const mySwiper = new Swiper('.swiper-container', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      slidesOffsetAfter: 0,
+    },
+    350: {
+      slidesPerView: 1.5,
+      slidesOffsetAfter: 0,
+    },
+    640: {
+      slidesPerView: 1.8,
+      slidesOffsetAfter: 0,
+    },
+    730: {
+      slidesPerView: 2,
+      slidesOffsetAfter: 0,
+    },
+    1000: {
+      slidesPerView: 3,
+      slidesOffsetAfter: 0,
+    },
+    1280: {
+      slidesPerView: 4,
+      slidesOffsetAfter: 150,
+    },
+  },
 
+  pagination: {
+    el: '.oms-about__list-pagination',
+  },
 
-
-
-
-
-
-
-
+  navigation: {
+    nextEl: '.oms-about__list-next',
+    prevEl: '.oms-about__list-prev',
+  },
+});
