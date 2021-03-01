f797529da7f5c4f2e3bf66b35a70f312fa5bfe3a
pervakam <52745442+pervakam@users.noreply.github.com>
2021-03-01 09:09:07 +0300

    добавляет файлы вёрстки вносит изменения

--- js/main.js
+++ js/main.js
@@ -308,16 +308,16 @@ if (window.innerWidth <= 1279) {
 }
 
 ///// скрывет окно с фильтром /////
-const tabSearchForm = document.querySelector('.tab__search');
+const tabSearchForm = document.querySelector('.tab-item__search');
 
 const tabFilterHandler = function () {
-  tabSearchForm.classList.toggle('tab__search--hide');
+  tabSearchForm.classList.toggle('tab-item__search--hide');
 };
 
 if (window.innerWidth <= 1279 && tabSearchForm) {
-  const tabFilterButton = document.querySelector('.tab__form-button');
+  const tabFilterButton = document.querySelector('.tab-item__form-button');
   tabFilterButton.addEventListener('click', tabFilterHandler);
-  const tabSearchCloseButton = document.querySelector('.tab__search-toggle');
+  const tabSearchCloseButton = document.querySelector('.tab-item__search-toggle');
   tabSearchCloseButton.addEventListener('click', tabFilterHandler);
 }
 
@@ -332,67 +332,190 @@ const PhotoSlider = new Swiper('.tab__article-photo-list', {
     nextEl: '.tab__article-photo-next',
     prevEl: '.tab__article-photo-prev',
   },
+});
 
-  slidesPerView: 1,
+/// карусель фотографий в разделе фотогалереи /////
+const tabPhotoSections = document.querySelectorAll('.tab__photo-section');
 
+tabPhotoSections.forEach(function (it) {
+  const swiperList = it.querySelector('.tab__photo-section-list');
+  const swiperNextEl = it.querySelector('.tab__photo-section-next');
+  const swiperPrevEl = it.querySelector('.tab__photo-section-prev');
+
+  const PhotoSectionSlider = new Swiper(swiperList, {
+    slidesPerView: 'auto',
+
+    wrapperClass: 'tab__photo-section-wrapper',
+
+    slideClass: 'tab__photo-section-slide',
+
+    pagination: {
+      el: '.tab__article-photo-counter',
+      type: 'fraction',
+    },
+
+    navigation: {
+      nextEl: swiperNextEl,
+      prevEl: swiperPrevEl,
+    },
+  });
 });
 
 ///// модалка с фото /////
-// const photoSectionOpenButtons = document.querySelectorAll('.tab__photo-section-button');
-//
-// const photoSectionOpenButtonsArray = Array.prototype.slice.call(photoSectionOpenButtons);
-//
-// photoSectionOpenButtonsArray.forEach(function (it) {
-//   let photoSection = it.parentElement.parentElement;
-//   let photoSectionWrapper = photoSection.parentElement;
-//   const photoSectionNavigation = photoSection.querySelector('.tab__photo-section-navigation')
-//
-//   it.addEventListener('click', function (it) {
-//     photoSection.classList.add('tab__photo-section--modal');
-//     body.classList.add('no-scroll');
-//     photoSectionWrapper.classList.add('tab__photo-section-overlay');
-//     photoSectionNavigation.classList.add('tab__photo-section-navigation--hide')
-//
-//     // const ModalPhotoSlider = new Swiper('.tab__article-photo-list', {
-//     //
-//     //   wrapperClass: 'tab__article-photo-wrapper',
-//     //
-//     //   slideClass: 'tab__article-photo-img',
-//     //
-//     //   navigation: {
-//     //     nextEl: '.tab__article-photo-prev',
-//     //     prevEl: '.tab__article-photo-next',
-//     //   },
-//     //
-//     // });
-//   });
-// });
-const galleryThumbs  = new Swiper('.list-small', {
-  wrapperClass: 'wrapper-small',
+const photoSectionOpenButtons = document.querySelectorAll('.tab__photo-section-button');
 
-  slideClass: 'slide-small',
+const photoSectionOpenButtonsArray = Array.prototype.slice.call(photoSectionOpenButtons);
 
-  spaceBetween: 4,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
+photoSectionOpenButtonsArray.forEach(function (it) {
+  const photoModalClone = it.parentElement.parentElement.parentElement.cloneNode(true)
+  const tabPhotoItem = it.parentElement.parentElement.parentElement.parentElement;
+
+  it.addEventListener('click', function () {
+    const photoModalOverlay = tabPhotoItem.appendChild(photoModalClone);
+    const photoModal = photoModalOverlay.querySelector('.tab__photo-section');
+    photoModalOverlay.classList.add('tab__photo-section-overlay');
+    photoModal.classList.add('tab__photo-section--modal');
+
+    const topList = photoModal.querySelector('.tab__photo-section-list');
+    const topWrapper = photoModal.querySelector('.tab__photo-section-wrapper');
+    const topSlides = photoModal.querySelectorAll('.tab__photo-section-slide');
+    topList.classList.add('tab__photo-section-list--top');
+    topWrapper.classList.add('tab__photo-section-wrapper--top')
+    topSlides.forEach(function (it) {
+      it.classList.add('tab__photo-section-slide--top')
+    });
+
+    const photoModalThumbs = photoModalOverlay.querySelector('.tab__photo-section-list').cloneNode(true);
+    const photoModalThumbsSection = photoModal.appendChild(photoModalThumbs);
+    const thumbsWrapper = photoModalThumbsSection.querySelector('.tab__photo-section-wrapper');
+    const thumbsSlides = photoModalThumbsSection.querySelectorAll('.tab__photo-section-slide');
+    photoModalThumbsSection.classList.add('tab__photo-section-list--thumbs');
+    thumbsWrapper.classList.add('tab__photo-section-wrapper--thumbs')
+    thumbsSlides.forEach(function (it) {
+      it.classList.add('tab__photo-section-slide--thumbs')
+    });
+
+    const galleryThumbs  = new Swiper('.tab__photo-section-list--thumbs', {
+      wrapperClass: 'tab__photo-section-wrapper--thumbs',
+      slideClass: 'tab__photo-section-slide--thumbs',
+      spaceBetween: 4,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      // navigation: {
+      //   nextEl: '.next',
+      //   prevEl: '.prev',
+      // },
+    });
+
+    const galleryTop  = new Swiper('.tab__photo-section-list--top', {
+      wrapperClass: 'tab__photo-section-wrapper--top',
+      slideClass: 'tab__photo-section-slide--top',
+      thumbs: {
+        swiper: galleryThumbs
+      },
+    });
+
+  });
+});
+
+///// слайдер новостей /////
+const tabSlides = document.querySelectorAll('.tab-item__list-slide');
+const tabListNavigation = document.querySelector('.tab-item__list-navigation');
+
+const newsSwiper = new Swiper('.tab-item__list-container', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1.007,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+  },
+
+  slidesPerColumn: 3,
+  spaceBetween: 24,
+
+  wrapperClass: 'tab-item__list-wrapper',
+
+  slideClass: 'tab-item__list-slide',
+
+  pagination: {
+    el: '.tab-item__list-pagination',
+    dynamicBullets: true,
+    bulletClass: 'tab-item__list-pagination-bullet',
+    bulletActiveClass: 'tab-item__list-pagination-bullet--active',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '' +
+        '<div class="' + className + '">' + (index + 1) + '</div>';
+    },
+  },
 
   navigation: {
-    nextEl: '.next',
-    prevEl: '.prev',
+    nextEl: '.tab-item__list-next',
+    prevEl: '.tab-item__list-prev',
   },
 });
 
-const galleryTop  = new Swiper('.list', {
-  slidesPerView: 1,
+if(tabSlides.length < 7 && tabListNavigation) {
+  tabListNavigation.classList.add('tab-item__list-navigation--hide')
+}
 
-  wrapperClass: 'wrapper',
+///// фотографии в карточке новостей /////
+const tabItemPhotosLists = document.querySelectorAll('.tab-item__list-slide-photos-list');
+const MAX_PHOTO_IN_CARD_DESKTOP = 4;
+const MAX_PHOTO_IN_CARD_TABLET = 3;
 
-  slideClass: 'slide',
+tabItemPhotosLists.forEach(function (it) {
+  const tabItemPhotosItems = it.querySelectorAll('.tab-item__list-slide-photos-item');
 
-  thumbs: {
-    swiper: galleryThumbs
+  const tabItemPhotosCounter = function (maxPhoto) {
+    tabItemPhotosItems[maxPhoto - 1].classList.add('tab-item__list-slide-photos-item--more');
+    tabItemPhotosItems[maxPhoto - 1].querySelector('.tab-item__list-slide-photos-item-pic').classList.add('tab-item__list-slide-photos-item-pic--hide');
+    tabItemPhotosItems[maxPhoto - 1].textContent = '+' + (tabItemPhotosItems.length - (maxPhoto - 1));
   }
 
+  if(tabItemPhotosItems.length > MAX_PHOTO_IN_CARD_DESKTOP && window.innerWidth > 1280) {
+    tabItemPhotosCounter(MAX_PHOTO_IN_CARD_DESKTOP);
+  } else if(tabItemPhotosItems.length > MAX_PHOTO_IN_CARD_TABLET && window.innerWidth <= 1279) {
+    tabItemPhotosCounter(MAX_PHOTO_IN_CARD_TABLET);
+  }
+})
+
+///// слайдер в фотогалерее /////
+const tabMediaSlides = document.querySelectorAll('.tab-item__media-slide');
+const tabMediaNavigation = document.querySelector('.tab-item__media-list-navigation');
+
+const MediaSwiper = new Swiper('.tab-item__media-list-container', {
+  slidesPerView: 1,
+  slidesPerColumn: 3,
+  spaceBetween: 24,
+  allowTouchMove: false,
+
+  wrapperClass: 'tab-item__media-list-wrapper',
+
+  slideClass: 'tab-item__media-slide',
+
+  pagination: {
+    el: '.tab-item__media-list-pagination',
+    dynamicBullets: true,
+    bulletClass: 'tab-item__media-list-pagination-bullet',
+    bulletActiveClass: 'tab-item__media-list-pagination-bullet--active',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '' +
+        '<div class="' + className + '">' + (index + 1) + '</div>';
+    },
+  },
+
+  navigation: {
+    nextEl: '.tab-item__media-list-next',
+    prevEl: '.tab-item__media-list-prev',
+  },
 });
+
+if(tabMediaSlides.length < 4) {
+  tabMediaNavigation.classList.add('tab-item__media-list-navigation--hide')
+}
